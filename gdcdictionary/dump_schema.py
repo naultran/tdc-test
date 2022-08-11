6d9d55cc81d62b8b842f8d510cc0d1c9ff5c5476
Nault, Rance <naultran@msu.edu>
2022-08-11 18:28:12 -0400

    it broke

--- gdcdictionary/dump_schema.py
+++ gdcdictionary/dump_schema.py
@@ -10,7 +10,7 @@ import sys
 #from gdcdictionary import SCHEMA_DIR
 from dictionaryutils import dump_schemas_from_dir
 
-SCHEMA_DIR="./schemas"
+SCHEMA_DIR="./original_schemas"
 print(SCHEMA_DIR)
 
 if len(sys.argv) > 1 :
